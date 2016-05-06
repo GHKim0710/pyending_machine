48f5049f10c2e1a4a357d9ace152bfcd42ba71a2
wesky93 <wesky93@gmail.com>
2016-05-06 23:06:34 +0900

    자판기 금액 투입 함수 추가
    돈을 입력 받고 입력값이 숫자인지 최소,최대값 사이인지 확인을 하고 문제 없으면 금액을 입력 받도록 구현

--- start_machin.py
+++ start_machin.py
@@ -7,17 +7,16 @@
 포가리 = {"num": 3, "name": "포가리", "price": 1000}
 menu = [사리다,콤디션,포가리]
 
-#==========================
-#화면 출력을 위한 함수
+# ==========================
+# 화면 출력을 위한 함수
 
 def dp_menu():
-    #자판기 화면에서 메뉴 부분을 출력해주는 함수
+    # 자판기 화면에서 메뉴 부분을 출력해주는 함수
     print("| 번호 |   제품명 |    가격 |")
     for can in menu:
         print("| {0:<4} | {1:>5} | {2:>5}원 |".format (can["num"],can["name"],can["price"]))
 
 def dp():
-    # [] 추후 자판기에 등록된 제품을 넘겨받아 디스플레이에 뿌려주도록 수정
     print("_____________________________")
     print("|   시원한 음료수 자판기    |")
     print("+======+==========+=========+")
@@ -25,8 +24,42 @@ def dp():
     print("+======+==========+=========+")
     print("-----------------------------")
 
-#==========================
+#==================================
 #제품 선택 및 잔돈 처리를 위한 함수
+
+def coin_min():
+    # 자판기에 있는 제품가격중 최소값을 구하는 함수
+    price_min = 0
+    for can in menu:
+        if price_min=0 or can["price"] < price_min:
+            price_min = can["price"]
+    return price_min
+
+def coin():
+    # 투입 가능한 돈의 최대,최소 값
+    co_min = coin_min()
+    co_max = 5000
+    # 돈을 입력받고 오류를 검증
+    while True:
+        coin = input("돈을 넣어주세요 :")
+        try:
+            coin= int(coin)
+        except:
+            print("숫자만 입력해주세요")
+        if coin > co_max:
+            print("최대 5000원까지만 넣어주세요")
+        elif coin < co_min:
+            print("최소 {0}원보다 더 넣어주세요")
+        else:
+            break
+        return coin
+
+def can():
+    pass
+
+def result():
+    pass
+
 
 
 dp()
