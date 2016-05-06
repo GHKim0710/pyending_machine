74973176ec82bcbfed7436ad226c0c358c8ef54f
wesky93 <wesky93@gmail.com>
2016-05-06 23:20:48 +0900

    coin 함수 오류 수정&can 숫자 리스트 생성 함수 추가
    coin함수 입력후 제대로 검증 되지 않은 부분 수정
    
    제품 선택시 올바른 값인지 확인하는 용도의 제품 숫자 리스트 생성 함수 추가

--- start_machin.py
+++ start_machin.py
@@ -1,11 +1,10 @@
 
-
-#간이 자판기 프로그램 입니다.
-#자판기에 들어가는 제품
+# 간이 자판기 프로그램 입니다.
+# 자판기에 들어가는 제품
 사리다 = {"num": 1, "name": "사리다", "price": 800}
 콤디션 = {"num": 2, "name": "콤디션", "price": 1500}
 포가리 = {"num": 3, "name": "포가리", "price": 1000}
-menu = [사리다,콤디션,포가리]
+menu = [사리다 ,콤디션 ,포가리]
 
 # ==========================
 # 화면 출력을 위한 함수
@@ -14,7 +13,7 @@ def dp_menu():
     # 자판기 화면에서 메뉴 부분을 출력해주는 함수
     print("| 번호 |   제품명 |    가격 |")
     for can in menu:
-        print("| {0:<4} | {1:>5} | {2:>5}원 |".format (can["num"],can["name"],can["price"]))
+        print("| {0:<4} | {1:>5} | {2:>5}원 |".format (can["num"] ,can["name"] ,can["price"]))
 
 def dp():
     print("_____________________________")
@@ -24,14 +23,15 @@ def dp():
     print("+======+==========+=========+")
     print("-----------------------------")
 
-#==================================
-#제품 선택 및 잔돈 처리를 위한 함수
+
+# ==================================
+# 제품 선택 및 잔돈 처리를 위한 함수
 
 def coin_min():
     # 자판기에 있는 제품가격중 최소값을 구하는 함수
     price_min = 0
     for can in menu:
-        if price_min=0 or can["price"] < price_min:
+        if price_min == 0 or can["price"] < price_min:
             price_min = can["price"]
     return price_min
 
@@ -43,16 +43,26 @@ def coin():
     while True:
         coin = input("돈을 넣어주세요 :")
         try:
-            coin= int(coin)
+            coin = int(coin)
         except:
             print("숫자만 입력해주세요")
+            continue
         if coin > co_max:
             print("최대 5000원까지만 넣어주세요")
+            continue
         elif coin < co_min:
             print("최소 {0}원보다 더 넣어주세요")
+            continue
         else:
             break
-        return coin
+    return coin
+
+def can_list():
+    # 제품의 숫자 목록을 반환하는 함수
+    num_list = []
+    for can in memu:
+        num_list.append(can["number"])
+    return num_list
 
 def can():
     pass
@@ -63,3 +73,4 @@ def result():
 
 
 dp()
+coin()
