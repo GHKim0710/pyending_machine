d2f094dc2b3bfbac9804b04c10580482c3bf7687
wesky93 <wesky93@gmail.com>
2016-05-14 10:08:20 +0900

    sqlite 아이템 추가 함수 제작중
    num 컬럼이 없다는 오류가 나와 수정필요

--- test_sqlit.py
+++ test_sqlit.py
@@ -1,6 +1,10 @@
 import sqlite3
 
 db = "test.db"
+def 아이템추가(num,음료명,재고):
+    with sqlite3.connect(db) as DB:
+        질의 = DB.cursor()
+        질의.execute('INSERT INTO item VALUES (num,음료명,재고)')
+        print("{1}을 {0}번에 추가하였습니다.".format(num,음료명))
 
-with sqlite3.connect(db):
-    
+아이템추가(3,"헉개수",10)
