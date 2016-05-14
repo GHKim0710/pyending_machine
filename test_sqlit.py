ca945c65bcb651d70cb3043a47235ef05fe7d051
wesky93 <wesky93@gmail.com>
2016-05-14 23:46:15 +0900

    sql 테스트중
    sql 함수 인설트가 되지 않아 중간에 확인 print문으로 오류 체크중
    현재 sql의 질의어에서 오류를 일으키는것으로 판단되서 버그 찾는중

--- test_sqlit.py
+++ test_sqlit.py
@@ -1,10 +1,13 @@
 import sqlite3
 
 db = "test.db"
-def 아이템추가(num,음료명,재고):
+def 아이템추가(번호: object, 음료명: object, 재고: object) -> object:
+    print("함수 시작",db)
     with sqlite3.connect(db) as DB:
+        print("DB연결")
         질의 = DB.cursor()
-        질의.execute('INSERT INTO item VALUES (num,음료명,재고)')
-        print("{1}을 {0}번에 추가하였습니다.".format(num,음료명))
+        print("커서 연결")
+        질의.execute('INSERT INTO item(num,name,count) VALUES (번호,음료명,재고)')
+        print("{1}을 {0}번에 추가하였습니다.".format(번호,음료명))
 
 아이템추가(3,"헉개수",10)
